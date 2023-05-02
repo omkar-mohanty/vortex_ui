7248d1fc229b6ea880e66145e139608bbf68d3c0
omkar-mohanty <franzohouser@gmail.com>
2023-05-02 11:11:04 +0530

    add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,80 @@
+mod extraction;
+
+use std::ops::RangeInclusive;
 use std::path::PathBuf;
 
-use iced::widget::{button, Column};
-use iced::widget::{column, container,text};
-use iced::{Alignment, Element, Length,Sandbox, Settings};
+use iced::widget::{button, progress_bar, Column};
+use iced::widget::{column, container, text};
+use iced::{Alignment, Element, Length, Settings, Application, executor, Theme, Command, Subscription};
 use native_dialog::FileDialog;
-struct Counter {
+struct Vortex {
     pdf_path: PathBuf,
+    state: AppState,
+}
+
+enum AppState {
+    Init,
+    Processing,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     FileSelect,
+    ExtractionProgress(f32)
 }
 
-impl Sandbox for Counter {
+impl Application for Vortex {
     type Message = Message;
+    type Executor = executor::Default;
+    type Flags = ();
+    type Theme = Theme;
 
-    fn new() -> Self {
-        Counter { pdf_path: PathBuf::new() }
+    fn new(_flags:()) -> (Vortex, Command<Message>) {
+        (Vortex {
+            pdf_path: PathBuf::new(),
+            state: AppState::Init,
+        },
+        Command::none())
     }
     fn title(&self) -> String {
         "Vortex UI".to_string()
     }
     fn view(&self) -> Element<Message> {
-        let welcome_text = text("Welcome to Vortex UI!");
+        match self.state {
+            AppState::Init => {
+                let welcome_text = text("Welcome to Vortex UI!");
 
-        let select_files_button = button("Select file")
-            .on_press(Message::FileSelect)
-            .padding(10);
+                let select_files_button = button("Select file")
+                    .on_press(Message::FileSelect)
+                    .padding(10);
 
-        let column: Column<_> = column![welcome_text, select_files_button]
-            .align_items(Alignment::Center)
-            .spacing(10);
+                let column: Column<_> = column![welcome_text, select_files_button]
+                    .align_items(Alignment::Center)
+                    .spacing(10);
 
-        container(column)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_x()
-            .center_y()
-            .into()
+                container(column)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_x()
+                    .center_y()
+                    .into()
+            }
+
+            AppState::Processing => {
+                let range_progress = RangeInclusive::new(0.0, 100.0);
+                let progress = progress_bar(range_progress, 50.0);
+
+                container(progress)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_y()
+                    .center_x()
+                    .into()
+            }
+        }
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::FileSelect => {
                 let path = FileDialog::new()
@@ -50,11 +83,21 @@ impl Sandbox for Counter {
                     .show_open_single_file()
                     .unwrap();
                 self.pdf_path = path.unwrap();
+                self.state = AppState::Processing;
             }
-        }
+            Message::ExtractionProgress(progress) => {
+
+            }
+        };
+
+        Command::none()
     }
 }
 
+pub struct Extraction {
+
+}
+
 fn main() {
-    Counter::run(Settings::default()).unwrap();
+    Vortex::run(Settings::default()).unwrap();
 }
